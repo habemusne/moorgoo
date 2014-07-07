88a8b2161becb08e2a9162d4ca0948750491a8c1
shumin0809 <shumin0809@yahoo.com>
2014-07-07 14:54:23 -0700

    fixed dropdown menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+
+
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require jquery
 //= require_tree .
 //= require jquery.validate
 //= require jquery.validate.additional-methods
