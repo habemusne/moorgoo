5319b6644139e930418ef33fe8b635de4ab5edea
coolmich <coolmich00@gmail.com>
2014-07-24 20:35:26 -0700

    fix school merging with chen and shu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,9 @@ class HomeController < ApplicationController
   end
 
   def campus
+    unless cookies.signed[:perm_school].nil?
+      redirect_to school_path(@school.name)
+    end
   end
   
 end
