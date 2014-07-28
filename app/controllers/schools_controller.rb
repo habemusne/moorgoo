6d824af7d63df4642fbc63f3c4788d8b3983cbf5
coolmich <coolmich00@gmail.com>
2014-07-27 22:40:49 -0700

    fix school and turbol link

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,5 +1,5 @@
 class SchoolsController < ApplicationController
   def show
-    cookies.signed[:perm_school] = { value: @school.id, expires: 20.years.from_now.utc}
+    #cookies.signed[:perm_school] = { value: @school.id, expires: 20.years.from_now.utc}
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ucsd = School.create(:name=>'ucsd', :slug=>'ucsd')
