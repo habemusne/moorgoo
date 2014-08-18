2d9f3f54217134cf62e75024337fb37084466731
Nan Chen <itisnanful@gmail.com>
2014-08-18 14:07:30 -0700

    refractored steal.py

--- fetchDataBundle/steal.py
+++ fetchDataBundle/steal.py
@@ -8,59 +8,44 @@ from bs4 import BeautifulSoup
 def fetchBook (school, fetch_wait_time = 1):
   driver = webdriver.Chrome('/Users/a67/bin/chromedriver')
   driver.get("https://www.postyourbook.com/" + str(school))
+  bookinfo = {}
+  bookname_arr = []
+  author_arr = []
+  ISBN10_arr = []
+  ISBN13_arr = []
+  seller_number_arr = []
+  most_recent_date_arr = []
+  student_lowest_price_arr = []
 
   while (True):
     #Wait for the data to be fetched and displayed
     time.sleep(fetch_wait_time)
+    book_counter = 0;
 
     # Get booknames
     booknames = driver.find_elements_by_xpath("//h3/a[@class='postingitem']")
     for bookname in booknames:
-      source_code = bookname.get_attribute("innerHTML")
+      book_counter = book_counter + 1
       # print source_code
 
-    # Get authors
     authors = driver.find_elements_by_class_name("p-authors")
-    for author in authors:
-      source_code = author.get_attribute("innerHTML").strip().split(':&nbsp;')[1]
-      # print source_code
-
-    # Get ISBN10 and ISBN13
     isbns = driver.find_elements_by_xpath("//div[@class='post-desc']/span")
-    ISBN10_arr = []
-    ISBN13_arr = []
-    for isbn in isbns:
-      source_code = isbn.get_attribute("innerHTML").strip().split(":&nbsp;")
+    seller_numbers = driver.find_elements_by_xpath("//span[@class='owner']/span")
+    most_recent_dates = driver.find_elements_by_xpath("//span[@class='clock']/span")
+    student_lowest_prices = driver.find_elements_by_class_name("post-price")
+    for i in range(0, book_counter):
+      bookname_arr.append(booknames[i].get_attribute("innerHTML"))
+      author_arr.append(authors[i].get_attribute("innerHTML").strip().split(':&nbsp;')[1])
+      source_code = isbns[i].get_attribute("innerHTML").strip().split(":&nbsp;")
       if source_code[0] == str("ISBN10"):
         ISBN10_arr.append(source_code[1])
       elif source_code[0] == str("ISBN13"):
         ISBN13_arr.append(source_code[1])
       else:
         print "ISBN fetch error"
-    # for i in ISBN10_arr:
-    #   print i
-    # for i in ISBN13_arr:
-    #   print i
-
-    # Get number of sellers
-    seller_numbers = driver.find_elements_by_xpath("//span[@class='owner']/span")
-    for seller_number in seller_numbers:
-      source_code = seller_number.get_attribute("innerHTML").strip().split("&nbsp")[0]
-      #print source_code
-
-    # Get most recent date of posting
-    most_recent_dates = driver.find_elemenqts_by_xpath("//span[@class='clock']/span")
-    for most_recent_date in most_recent_dates:
-      source_code = most_recent_date.get_attribute("innerHTML").strip()
-      #print source_code
-
-    # Get student's lowest posting price
-    student_lowest_prices = driver.find_elements_by_class_name("post-price")
-    for student_lowest_price in student_lowest_prices:
-      source_code = student_lowest_price.get_attribute("innerHTML").split("from")[1].strip()
-      print source_code
-
-    # 
+      seller_number_arr.append(seller_numbers[i].get_attribute("innerHTML").strip().split("&nbsp")[0])
+      most_recent_date_arr.append(most_recent_dates[i].get_attribute("innerHTML").strip())
+      student_lowest_price_arr.append(student_lowest_prices[i].get_attribute("innerHTML").split("from")[1].strip())
 
     # Wait for the "next page" button to show up. Solves ElementNotVisibleException
     time.sleep(fetch_wait_time)
