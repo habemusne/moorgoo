f176624af7392a9c329dae8344cd77d35e0f3c40
coolmich <coolmich00@gmail.com>
2014-09-23 11:09:32 +0800

    not finished

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,8 +1,8 @@
 class DataController < ApplicationController
   def index
     fourMA = threeMA = twoMA = oneMA = zeroMA = 0
-    threeWA = twoWA = oneWA = zeroWA = 0
-    sixDA = fiveDA = fourDA = threeDA = twoDA = oneDA = zeroDA = 0
+    fourWA = threeWA = twoWA = oneWA = zeroWA = 0
+    fourDA = threeDA = twoDA = oneDA = zeroDA = 0
     Bookprice.all.each do |b|
       if b.created_at > 1.month.ago
         zeroMA += 1
@@ -16,19 +16,17 @@ class DataController < ApplicationController
             twoDA += 1
           elsif b.created_at > 4.day.ago
             threeDA += 1
-          elsif b.created_at > 5.day.ago
-            fourDA += 1
-          elsif b.created_at > 6.day.ago
-            fiveDA += 1
           else
-            sixDA += 1
+            fourDA += 1
           end
         elsif b.created_at > 2.week.ago
           oneWA += 1
         elsif b.created_at > 3.week.ago
           twoWA += 1
-        else
+        elsif b.created_at > 4.week.ago
           threeWA += 1
+        else
+          fourWA += 1
         end
       elsif b.created_at > 2.month.ago
         oneMA += 1
@@ -41,8 +39,8 @@ class DataController < ApplicationController
       end
     end
     @season_incre = [fourMA, threeMA, twoMA, oneMA, zeroMA]
-    @month_incre = [threeWA, twoWA, oneWA, zeroWA]
-    @week_incre = [sixDA, fiveDA, fourDA, threeDA, twoDA, oneDA, zeroDA]
+    @month_incre = [fourWA, threeWA, twoWA, oneWA, zeroWA]
+    @week_incre = [fourDA, threeDA, twoDA, oneDA, zeroDA]
     @season_linear = Datum.increToLinear(@season_incre, 0)
     @month_linear = Datum.increToLinear(@month_incre, @season_linear[-2])
     @week_linear = Datum.increToLinear(@week_incre, @month_linear[-2])
