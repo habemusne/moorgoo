2a4c8915f5b183527c6505ff9eb62d1ca4be3734
coolmich <coolmich00@gmail.com>
2015-01-15 08:15:37 -0800

    add production email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,18 @@ Moorgoo::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'www.moorgoo.com' }
+  config.action_mailer.smtp_settings = {:address => "www.moorgoo.com", :port => 1025}
+
+  config.action_mailer.smtp_settings = {
+     :address              => "smtp.gmail.com",
+     :port                 => 587,
+     :user_name            => 'moorgoo2014@gmail.com',
+     :password             => 'moorgoo123',
+     :authentication       => :plain,
+     :enable_starttls_auto => true  }
 end
