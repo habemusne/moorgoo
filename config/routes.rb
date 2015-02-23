71313ed0d7130ddbe3c39e5b12e94a8c1c085d1e
Nan Chen <itisnanful@gmail.com>
2015-02-23 12:09:06 -0800

    cse11 quiz4 by zhang zi tong

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Moorgoo::Application.routes.draw do
   get 'cse11_quiz2_wi15' => 'home#cse11_quiz2_wi15'
   get 'cse11_quiz3_1_wi15' => 'home#cse11_quiz3_1_wi15'
   get 'cse11_quiz3_2_wi15' => 'home#cse11_quiz3_2_wi15'
+  get 'cse11_quiz4_2_wi15' => 'home#cse11_quiz4_2_wi15'
   get 'cse11_m_wi15' => 'home#cse11_m_wi15'
   get 'cse100_m_wi15' => 'home#cse100_m_wi15'
   get 'mmw12_pp1_wi15' => 'home#mmw12_pp1_wi15'
